7aee80e86f20b520ddb172dcef499db9dd985ff8
Salvatore Bonavita <salvo.bonavita9808@gmail.com>
2026-05-18 00:47:43 +0200

    aggiornamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Contact } from "@/components/site/Contact";
 import { FaqSection } from "@/components/site/FaqSection";
 import { Footer } from "@/components/site/Footer";
 import { Hero } from "@/components/site/Hero";
-import { LaunchOfferBanner } from "@/components/site/LaunchOfferBanner";
 import { Navbar } from "@/components/site/Navbar";
+import { PartnersMarquee } from "@/components/site/PartnersMarquee";
 import { Packages } from "@/components/site/Packages";
 import { Portfolio } from "@/components/site/Portfolio";
 import { Services } from "@/components/site/Services";
@@ -15,7 +15,7 @@ export default function Home() {
       <Navbar />
       <main className="flex-1">
         <Hero />
-        <LaunchOfferBanner />
+        <PartnersMarquee />
         <Services />
         <TalentBoard />
         <Portfolio />
